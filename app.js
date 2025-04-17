fa8fa9f3a5133b03d03370616f6c7ff137fd6c61
Mohammed Nifli <mohammednifliap@gmail.com>
2025-04-17 07:28:59 +0530

    feat(streams): create and explore streams in Node.js
    - Implemented and explored the use of streams for efficient data handling
    - Added examples for readable and writable streams
    - Improved performance by processing data in chunks rather than loading it all at once

--- app.js
+++ app.js
@@ -1,66 +1,91 @@
+import fs from "fs";
+
+const readableStream = fs.createReadStream("input.txt", { encode: "utf8" });
+
+const writableStream = fs.createWriteStream("output.txt");
+
+import express from "express";
+import { Worker } from "worker_threads";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 
 
-import fs from 'fs';
-
-const readableStream = fs.createReadStream('input.txt', { encode: 'utf8' })
-
-const writableStream = fs.createWriteStream('output.txt')
+// streams
 
 
 
-import express from 'express'
-import { Worker } from 'worker_threads'
 
+readableStream.on('data',(chunk)=>{
+    console.log("recived chunk",chunk.toString())
 
-const app = express()
-const port = 8000
+})
 
+readableStream.on('end',()=>{
+    console.log("No more data to read",)
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
 })
 
 
+writableStream.write("Hello ")
+writableStream.write("world!\n")
+
+writableStream.end();
+
+writableStream.on('finish', () => {
+    console.log('All data written to file.');
+  });
+
+
+readableStream.pipe(writableStream)
+
+readableStream.on('end',()=>{
+    console.log("file copied succesfully")
+})
+
+
+
+
+
+//worker threads
+
 function runworker(workerData) {
-    return new Promise((resolve, reject) => {
-        const worker = new Worker('./worker.js', { workerData })
-        worker.on('message', (message) => {
-            console.log('Worker successfully completed');
-            resolve(message);
-        });
-        worker.on('error', (error) => {
-            console.error('Something badly happened:', error);
-            reject(error);
-        });
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./worker.js", { workerData });
+    worker.on("message", (message) => {
+      console.log("Worker successfully completed");
+      resolve(message);
+    });
+    worker.on("error", (error) => {
+      console.error("Something badly happened:", error);
+      reject(error);
+    });
 
-        worker.on('exit', (code) => {
-            if (code !== 0) {
-                reject(new Error(`Worker stopped with exit code ${code}`));
-            }
-        });
-
-    })
-
+    worker.on("exit", (code) => {
+      if (code !== 0) {
+        reject(new Error(`Worker stopped with exit code ${code}`));
+      }
+    });
+  });
 }
 
 async function main() {
-    try {
-        const result = await runworker({ iterations: 1e7 })
+  try {
+    const result = await runworker({ iterations: 1e7 });
 
-        console.log('run worker', result)
-
-    } catch (error) {
-        console.log("error from worker", error)
-    }
+    console.log("run worker", result);
+  } catch (error) {
+    console.log("error from worker", error);
+  }
 }
 
-main()
-
-
+main();
 
 app.listen(8000, () => {
-    console.log(`server listening at http://localhost:${port}`)
-})
-
-
+  console.log(`server listening at http://localhost:${port}`);
+});
